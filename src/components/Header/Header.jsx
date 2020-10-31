52789cc05ff43408747360f69e4938855a1274ef
John Alonso <jvalon9455@gmail.com>
2020-10-31 13:07:26 -0400

    cleaning up. Starting sort by first name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,7 @@ const Header = (props) => {
     <div className={"jumbotron"}>
       <h1>Employee Directory</h1>
       <h6>
-        Click on each heading to filter or use search box to narrow employee
-        results
+       Enter first name of employee to search for colleague
       </h6>
       <input
         type="text"
