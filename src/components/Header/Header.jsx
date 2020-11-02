ac3d34c34ee395029f5c968850d83dd78878d4ed
John Alonso <jvalon9455@gmail.com>
2020-11-01 20:17:19 -0500

    added styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
     <div className={"jumbotron"}>
       <h1>Employee Directory</h1>
       <h6>
-       Enter first name of employee to search for colleague
+       Enter first name of employee to search for colleague, or click on up/down button to sort through first name
       </h6>
       <input
         type="text"
